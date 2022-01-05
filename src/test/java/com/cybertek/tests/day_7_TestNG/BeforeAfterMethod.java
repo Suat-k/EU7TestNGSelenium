eb2ab5c3b63cfd13ce8c5d39f246d5fe2ffcd7f1
Suat <suatk1602@gmail.com>
2022-01-05 03:42:50 +0000

    1st

---
+++ src/test/java/com/cybertek/tests/day_7_TestNG/BeforeAfterMethod.java
@@ -0,0 +1,47 @@
+package com.cybertek.tests.day_7_TestNG;
+
+import org.testng.annotations.*;
+
+public class BeforeAfterMethod {
+
+    @BeforeClass
+    public void cool() {
+        System.out.println("print before");
+        System.out.println("class");
+    }
+
+
+    @Test
+
+    public void Test3 () {
+        System.out.println(" lets do it");
+
+
+    }
+
+    @Test
+
+    public void test4 () {
+
+        System.out.println("completed");
+    }
+
+    @BeforeMethod
+    public void Attmp1 () {
+
+        System.out.println("WebDriver, opening browser");
+    }
+    @AfterMethod
+    public void Return () {
+        System.out.println("Closing browser");
+    }
+    @AfterClass
+
+    public void ending() {
+
+        System.out.println("ending with my apologies");
+    }
+
+
+
+}
